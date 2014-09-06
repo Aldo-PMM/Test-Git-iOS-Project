f92e7de31040a1fe694c4a3c7b114b93f4ac27ef
Aldo Jimenez <aldojimenez@Aldos-MacBook-Pro.local>
2014-09-06 12:05:57 -0500

    added a test class

---
+++ PMMTest.h
@@ -0,0 +1,13 @@
+//
+//  PMMTest.h
+//  Git Test
+//
+//  Created by Aldo Jimenez on 9/6/14.
+//  Copyright (c) 2014 Proveedora Mexicana de Monofilamentos, S.A. de C.V. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMMTest : NSObject
+
+@end
---
+++ PMMTest.m
@@ -0,0 +1,13 @@
+//
+//  PMMTest.m
+//  Git Test
+//
+//  Created by Aldo Jimenez on 9/6/14.
+//  Copyright (c) 2014 Proveedora Mexicana de Monofilamentos, S.A. de C.V. All rights reserved.
+//
+
+#import "PMMTest.h"
+
+@implementation PMMTest
+
+@end
